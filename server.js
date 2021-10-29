2d0ebce533c298118cfd3633b45189918acaaa4c
Sanjay Sarkar <sanjay.sarkar.kol@xelpmoc.in>
2021-10-29 13:18:03 +0530

    first commit

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express()
-app.use(express.static(__dirname+'/dist/loginauthentication'))
+app.use(express.static(__dirname+'/dist/loginuser'))
 app.get('**',(req,res)=>{
-    return res.sendFile(__dirname+'/dist/loginauthentication/index.html')
+    return res.sendFile(__dirname+'/dist/loginuser/index.html')
 })
 app.listen(process.env.PORT || 8080,()=>{
     console.log('server is running.')
