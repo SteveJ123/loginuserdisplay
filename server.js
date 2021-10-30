2027c124affffe709721b7f307e99cb9bca2ef4f
Sanjay Sarkar <sanjay.sarkar.kol@xelpmoc.in>
2021-10-30 09:19:55 +0530

    user component

--- server.js
+++ server.js
@@ -1,59 +1,12 @@
-// const express = require('express');
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const port =  process.env.PORT || 8080;
-const userSchema = require('../backend/models/User');
-const userRoute = require('../backend/api/userRouter');
-// const app = express();
-const app = express();
-app.use(cors());
-
-
-
-
+const express = require('express')
+const app = express()
 app.use(express.static(__dirname+'/dist/loginuser'))
 app.get('**',(req,res)=>{
     return res.sendFile(__dirname+'/dist/loginuser/index.html')
 })
-
-
-const bodyParser = require('body-parser')
-
-app.use(bodyParser.json());
-
-app.get('/', (req,res)=>{
-    console.log("home page");
-    res.send("hello");
-});
-app.use('/users', userRoute);
-
- mongoose.connect(process.env.mongodb_uri,{
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-     }
-).then( ()=>{
-    // async
-    console.log("connected to db");
-    // const user = {
-    //     username: "user2",
-    //     password: 'test'
-    // }
-    // await new userSchema(user).save();
-}).catch(err=>console.log(err))
-
-
-// app.listen(port,()=>{
-//     console.log("app running");
-// })
-
-
-
 app.listen(process.env.PORT || 8080,()=>{
     console.log('server is running.')
 })
-
-
 
 
 
